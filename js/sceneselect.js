14e79c217d33f90aedbcfb776ccaef9ec81e22eb
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-05-10 14:56:01 +0100

    Blah

--- js/sceneselect.js
+++ js/sceneselect.js
@@ -1,10 +1,26 @@
 
+let scenes = [
+    {
+        "scene": 'rain_on_leaves',
+        "video": 'rain_on_leaves',
+        "sfx": 'rain_on_leaves'
+    }
+]
 
-function changeScene(source) {
-    document.getElementById("sfx").src = "./sfx/" + source + ".mp3";
+
+
+
+function changeScene(input) {
+
+    document.getElementById("infoPanel").style.display = "none";
+
+    let scene = scenes.filter((item) => {
+        return item["scene"] === input
+    })[0]
+
+    document.getElementById("sfx").src = "./sfx/" + scene["sfx"] + ".mp3";
     document.getElementById("music").play()
     document.getElementById("sfx").play()
-    document.getElementById("video").src = "./video/" + source + ".mp4";
+    document.getElementById("video").src = "./video/" + scene["video"] + ".mp4";
     document.getElementById("video").play()
-    document.getElementById("infoPanel").style.display = "none";
 }
