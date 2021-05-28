faa3cc06a275c0f439f56f82ec92e822b639c36f
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-05-28 17:00:37 +0100

    render in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+/* Global Imports */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'React with Webpack and Babel';
+/* Local Imports */
+
+class App extends React.Component {
+  render() {
+    return(
+      <div id="app">
+          <video autoplay loop muted id="video" type="video/mp4">
+              <source src="" type="video/mp4" />
+          </video>
+          
+          <div id="infoPanel">
+              <div id="infoText">
+                  <h1>Headphones Recommended</h1>
+                  <i class="fas fa-headphones"></i>
+              </div>
+              
+          </div>
+
+          <div id='container'>
+
+          </div>
+
+          <div class="controls">
+              <div class="sceneSelect">
+                  <button onClick="goButton()" id='gobutton'>GO</button>
+              </div>
+          </div>
+
+
+          <div class="footer">
+              <a href="https://ganeshh123.github.io">Ganesh H</a>
+          </div>
+      </div>
+    );
+  }
+}
  
 ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
+  <App />,
+  document.querySelector('body')
 );
 
 module.hot.accept()
