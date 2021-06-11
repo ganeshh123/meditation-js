eff88d5b76accbd3118293976515973881f74581
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2021-06-11 17:02:19 +0100

    blurry images

--- src/scenemedia/SceneImage.jsx
+++ src/scenemedia/SceneImage.jsx
@@ -9,7 +9,7 @@ export default class SceneVideo extends React.Component{
     constructor(props){
         super(props)
 
-        this.sceneVideoColors = {
+        this.sceneImageColors = {
 
         }
 
