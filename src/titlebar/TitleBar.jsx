0b96fc9f22d59c115cb1334bcfbc0d89159d3f6a
ganeshh123 <ganeshh123@users.noreply.github.com>
2021-11-07 15:24:30 +0000

    titlebar use svg icons

--- src/titlebar/TitleBar.jsx
+++ src/titlebar/TitleBar.jsx
@@ -1,74 +1,74 @@
 import React from 'react';
 
+import { AboutIcon, SettingsIcon } from '../icons';
+
 import './titleBar.scss'
 
 export default class TitleBar extends React.Component {
 
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props)
+  }
+
+  setStyle = () => {
+    this.titleBarStyle = {
+      backgroundColor: this.props.appState.currentTheme.backgroundColor,
+      border: this.props.appState.currentTheme.border,
+      boxShadow: this.props.appState.currentTheme.boxShadow,
+      backdropFilter: this.props.appState.currentTheme.backdropFilter,
+      WebKitBackdropFilter: this.props.appState.currentTheme.webkitBackdropFilter,
+      color: this.props.appState.currentTheme.accentColor
     }
 
-    setStyle = () => {
-        this.titleBarStyle = {
-          backgroundColor: this.props.appState.currentTheme.backgroundColor,
-          border: this.props.appState.currentTheme.border,
-          boxShadow: this.props.appState.currentTheme.boxShadow,
-          backdropFilter : this.props.appState.currentTheme.backdropFilter,
-          WebKitBackdropFilter : this.props.appState.currentTheme.webkitBackdropFilter,
-          color: this.props.appState.currentTheme.accentColor
-        }
-
-        this.titleBarIconStyle = {
-            filter: this.props.appState.currentTheme.iconColor
-        }
+    this.titleBarIconStyle = {
+      color: this.props.appState.currentTheme.accentColor
     }
+  }
 
-    settingsButtonPressed = () => {
-        let appState = this.props.appState
+  settingsButtonPressed = () => {
+    let appState = this.props.appState
 
-        appState.setStateFunction({
-          settingsShowing: true
-        })
-    }
+    appState.setStateFunction({
+      settingsShowing: true
+    })
+  }
 
-    appTitlePressed = () => {
-      let appState = this.props.appState
+  appTitlePressed = () => {
+    let appState = this.props.appState
 
-      appState.setStateFunction({
-        firstTime: true,
-        launchShowing: true
-      })
-    }
-  
-    render = () => {
-      this.setStyle()
+    appState.setStateFunction({
+      firstTime: true,
+      launchShowing: true
+    })
+  }
 
-      return(
-        <div id="titleBar" className="glassBlock" style={this.titleBarStyle}>
-          <h1 id="appTitleText" onClick={this.appTitlePressed}>{this.props.appTitleText}</h1>
-          <div id="buttonsHolder">
-                <img 
-                  src={"./assets/icons/about_icon.svg"} 
-                  style={this.titleBarIconStyle}
-                  id="aboutButton"
-                  className="iconButton"
-                  title="About"
-                  tabIndex="0"
-                  accessKey="i"
-                />
-                <img 
-                  src={"./assets/icons/settings_icon.svg"} 
-                  style={this.titleBarIconStyle}
-                  id="settingsButton"
-                  className="iconButton"
-                  title="Settings"
-                  tabIndex="0"
-                  accessKey="s"
-                  onClick={this.settingsButtonPressed}
-                />
-          </div>
+  render = () => {
+    this.setStyle()
+
+    return (
+      <div id="titleBar" className="glassBlock" style={this.titleBarStyle}>
+        <h1 id="appTitleText" onClick={this.appTitlePressed}>{this.props.appTitleText}</h1>
+        <div id="buttonsHolder">
+          <AboutIcon 
+            style={this.titleBarIconStyle}
+            id="aboutButton"
+            className="iconButton titleBarIcon"
+            title="About"
+            tabIndex="0"
+            accessKey="i"
+          />
+          <SettingsIcon
+            style={this.titleBarIconStyle}
+            id="settingsButton"
+            className="iconButton titleBarIcon"
+            title="Settings"
+            tabIndex="0"
+            accessKey="s"
+            onClick={this.settingsButtonPressed}
+          />
         </div>
-      );
-    }
+      </div>
+    );
+  }
 
 }
