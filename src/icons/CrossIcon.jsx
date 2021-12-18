115003e45a6d3d93b5f2dffabd30f9b8292ba700
ganeshh123 <ganeshh123@users.noreply.github.com>
2021-12-18 13:26:05 +0000

    refactor: re-exported cross icon

--- src/icons/CrossIcon.jsx
+++ src/icons/CrossIcon.jsx
@@ -3,20 +3,36 @@ import * as React from "react";
 function SvgCrossIcon({ title, titleId, ...props }) {
   return (
     <svg
+      data-name="Group 184"
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
+      viewBox="0 0 23 23"
       aria-labelledby={titleId}
       {...props}
     >
       {title ? <title id={titleId}>{title}</title> : null}
+      <defs>
+        <clipPath id="cross_icon_svg__a">
+          <path
+            data-name="Rectangle 94"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            d="M0 0h23v23H0z"
+          />
+        </clipPath>
+      </defs>
       <g
+        data-name="Group 183"
+        clipPath="url(#cross_icon_svg__a)"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
-        strokeWidth={3}
+        strokeWidth={2}
       >
-        <path d="M1 1l40 40M41 1L1 41" />
+        <path data-name="Line 182" d="M1.02 1.02l20.408 20.408" />
+        <path data-name="Line 183" d="M21.428 1.02L1.02 21.428" />
       </g>
     </svg>
   );
