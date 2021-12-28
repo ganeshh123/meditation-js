8f11ff748bc33f66705c4cf027e1cdfee5c2bada
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2021-12-28 15:26:25 +0000

    feat: upgrade webpack and tailwind to latest, clean package config - [x]  Upgrade Tailwind to Version 3 - [x]  Simplify Webpack Setup - [x]  Remove unused CSS in production - [x]  Remove unused packages

--- src/webpack-web.config.js
+++ src/webpack-web.config.js
@@ -1,49 +1,80 @@
-let webpack = require('webpack')
 let path = require('path')
-
 let MiniCssExtractPlugin = require('mini-css-extract-plugin')
 let CopyPlugin = require("copy-webpack-plugin")
 
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
     // Entry Location
     entry: path.resolve(__dirname, 'App.js'),
     // Use Babel to Convert to backwards compatible JS
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(scss|css)$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
-        }
-      ]
+        rules: [
+            {
+                // Use Babel to Convet React to Vanilla JS
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                // Instructions to process SASS and CSS
+                test: /\.(scss|css)$/,
+                use: [
+                    // Extract CSS from JS into Stylesheet
+                    MiniCssExtractPlugin.loader,
+                    // Load CSS
+                    'css-loader',
+                    // Use PostCSS to load Tailwind Classes
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: {
+                                    tailwindcss: {
+                                        config: path.resolve(__dirname, 'tailwind.config.js')
+                                    },
+                                    autoprefixer: {},
+                                    ...(isProduction ? {} : {})
+                                }
+                            }
+                        }
+                    },
+                    // Convert SASS to CSS
+                    'sass-loader'
+                ],
+            }
+        ]
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx'],
+        extensions: ['*', '.js', '.jsx'],
     },
-    // Output to dist/web folder
     output: {
-      path: path.resolve(__dirname, '../dist/web'),
-      filename: 'webapp.js'
+        // Output to dist/web folder
+        path: path.resolve(__dirname, '../dist/web'),
+        filename: 'webapp.js'
     },
-    // Enable React Hot Loading
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new MiniCssExtractPlugin(),
-      new CopyPlugin({
-        patterns: [
-          { from: path.resolve(__dirname, '../assets'), to: path.resolve(__dirname, '../dist/web/assets/') },
-          { from: path.resolve(__dirname, './web/index.html'), to: path.resolve(__dirname, '../dist/web/index.html') }
-        ],
-      })
+        // Extract CSS from JS
+        new MiniCssExtractPlugin(),
+        // Copy assets to dist dir
+        new CopyPlugin({
+            patterns: [
+                {from: path.resolve(__dirname, '../assets'), to: path.resolve(__dirname, '../dist/web/assets/')},
+                {
+                    from: path.resolve(__dirname, './web/index.html'),
+                    to: path.resolve(__dirname, '../dist/web/index.html')
+                }
+            ],
+        })
     ],
     // Load static assets from dist/web folder
     devServer: {
-      writeToDisk: true,
-      contentBase: path.resolve(__dirname, '..', 'dist', 'web')
+        static: {
+            directory: path.resolve(__dirname, '..', 'dist', 'web')
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+        hot: true
     }
-  };
+};
