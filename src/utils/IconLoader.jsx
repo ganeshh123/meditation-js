0d60d92619427c4d234081c99e32de449978d248
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2022-05-29 10:46:35 +0100

    chore: import new icons into IconLoader

--- src/utils/IconLoader.jsx
+++ src/utils/IconLoader.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import {SolidGlassIcon, BellIcon, BellOffIcon, PinIcon, NewTimerIcon} from "../icons";
+import {
+    SolidGlassIcon,
+    BellIcon,
+    BellOffIcon,
+    PinIcon,
+    NewTimerIcon,
+    HideTimeIcon,
+    EyeIcon,
+    LargeBellIcon
+} from "../icons";
 
 
 export const IconLoader = (props) => {
     const {iconName, iconProps} = props
 
-    return(
+    return (
         <>
             {iconName === 'SolidGlassIcon' && <SolidGlassIcon {...iconProps} />}
             {iconName === 'BellIcon' && <BellIcon {...iconProps} />}
             {iconName === 'BellOffIcon' && <BellOffIcon {...iconProps} />}
             {iconName === 'PinIcon' && <PinIcon {...iconProps} />}
             {iconName === 'NewTimerIcon' && <NewTimerIcon {...iconProps}/>}
+            {iconName === 'HideTimeIcon' && <HideTimeIcon {...iconProps} />}
+            {iconName === 'EyeIcon' && <EyeIcon {...iconProps} />}
+            {iconName === 'LargeBellIcon' && <LargeBellIcon {...iconProps} />}
         </>
     )
 }
